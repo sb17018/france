da4ec1069e51e91ae2f3a8ac9b61182e0be415b4
sb17018 <piotr.a.bar@gmail.com>
2025-12-26 20:22:39 +0100

    Amend folder structure

---
+++ js/coordinates.js
@@ -0,0 +1,110 @@
+const COORDINATES = [
+    {"Biarritz":{ 
+        region: "Nouvelle-Aquitaine",
+        department: "Pyrénées-Atlantiques",
+        iata: "BIQ",
+        latitude:{direction:"n",degree:43,minute:28,second:48,"total-seconds":156528}, 
+        longitude:{direction:"w",degree:1,minute:33,second:20,"total-seconds":5600},
+        x: 275,
+        y: 520
+    }},
+    {"Carcassonne": {
+        region: "Occitanie",
+        department: "Aude",
+        iata: "CCF",
+        latitude:{direction:"n",degree:43,minute:13,second:0,"total-seconds":155580},
+        longitude:{direction:"e",degree:2,minute:21,second:0,"total-seconds":8460},
+        x: 470,
+        y: 540
+    }},
+    {"Grenoble":{
+        region: "Auvergne-Rhône-Alpes",
+        department: "Isère",
+        iata: "GNB",
+        latitude:{direction:"n",degree:45,minute:10,second:18,"total-seconds":162618},
+        longitude:{direction:"e",degree:5,minute:43,second:21,"total-seconds":20601},
+        x: 640,
+        y: 405
+    }},
+    {"La Rochelle": {
+        region: "Nouvelle-Aquitaine",
+        department: "Charente-Maritime",
+        iata: "LRH",
+        latitude:{direction:"n",degree:46,minute:10,second:0,"total-seconds":166200},
+        longitude:{direction:"w",degree:1,minute:9,second:0,"total-seconds":4140},
+        x: 300,
+        y: 340
+    }},
+    {"Lourdes": {
+        region: "Occitanie",
+        department: "Hautes-Pyrénées",
+        iata: "LDE",
+        latitude:{direction:"n",degree:43,minute:5,second:39,"total-seconds":155139},
+        longitude:{direction:"e",degree:0,minute:2,second:49,"total-seconds":169},
+        x: 365,
+        y: 545
+    }},
+    {"Marseille": {
+        region: "Provence-Alpes-Côte-d-Azur",
+        department: "Bouches-du-Rhône",
+        iata: "MRS",
+        latitude:{direction:"n",degree:43,minute:17,second:47,"total-seconds":155867},
+        longitude:{direction:"e",degree:5,minute:22,second:12,"total-seconds":19332},
+        x: 620,
+        y: 535
+    }},
+    {"Nantes": {
+        region: "Pays-de-la-Loire",
+        department: "Loire-Atlantique",
+        iata: "NTE",
+        latitude:{direction:"n",degree:47,minute:13,second:5,"total-seconds":169985},
+        longitude:{direction:"w",degree:1,minute:33,second:10,"total-seconds":5590},
+        x: 280,
+        y: 260
+    }},
+    {"Nice": {
+        region: "Provence-Alpes-Côte-d-Azur",
+        department: "Alpes-Maritimes",
+        iata: "NCE",
+        latitude:{direction:"n",degree:43,minute:42,second:12,"total-seconds":157332},
+        longitude:{direction:"e",degree:7,minute:15,second:59,"total-seconds":26159},
+        x: 725,
+        y: 490
+    }},
+    {"Nimes": {
+        region: "Occitanie",
+        department: "Gard",
+        iata: "FNI",
+        latitude:{direction:"n",degree:43,minute:50,second:18,"total-seconds":157818},
+        longitude:{direction:"e",degree:4,minute:21,second:35,"total-seconds":15695},
+        x: 560,
+        y: 500
+    }},
+    {"Paris Beauvais": {
+        region: "Hauts-de-France",
+        department: "Oise",
+        iata: "BVA",
+        latitude:{direction:"n",degree:49,minute:25,second:59,"total-seconds":177959},
+        longitude:{direction:"e",degree:2,minute:4,second:59,"total-seconds":7499},
+        x: 465,
+        y: 140
+    }},
+    {"Rodez": {
+        region: "Occitanie",
+        department: "Aveyron",
+        iata: "RDZ",
+        latitude:{direction:"n",degree:44,minute:21,second:2,"total-seconds":159662},
+        longitude:{direction:"e",degree:2,minute:34,second:30,"total-seconds":9270},
+        x: 480,
+        y: 460
+    }},
+    {"Toulouse": {
+        region: "Occitanie",
+        department: "Haute-Garonne",
+        iata: "TLS",
+        latitude:{direction:"n",degree:43,minute:36,second:16,"total-seconds":156976},
+        longitude:{direction:"e",degree:1,minute:26,second:38,"total-seconds":5198},
+        x: 425,
+        y: 515
+    }}
+];
